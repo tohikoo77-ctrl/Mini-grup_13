dbaee3669d272e7265f36aba5566faaaceb5caa6
Твоё Имя <@example.com>
2026-06-02 18:51:22 +0500

    fix

--- commerce_extras/apps.py
+++ commerce_extras/apps.py
@@ -4,18 +4,5 @@ from django.apps import AppConfig
 class CommerceExtrasConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "commerce_extras"
-
-    def ready(self):
-        from rest_framework.views import APIView
-
-        from . import views
-
-        for view in vars(views).values():
-            if not isinstance(view, type):
-                continue
-
-            view_name = view.__name__.lower()
-            if ("news" in view_name or "discount" in view_name) and issubclass(view, APIView):
-                view.http_method_names = ["get", "head", "options"]
     label = "commerce_extras"
     verbose_name = "Commerce extras"
